ab00da5f81f5b956397fa48df302d677e78ad53b
tomasz-worobiec <tomasz.worobiec@protonmail.com>
2024-11-24 19:20:42 +0100

    [treesitter] Initialize plugin

---
+++ lua/tom/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+return {
+    'nvim-treesitter/nvim-treesitter',
+	commit = 'a3ca2e60563064cea008555d0b7d031430305b09',
+	config = function()
+		require('nvim-treesitter.configs').setup {
+			ensure_installed = {
+				'c',
+				'cpp',
+				'c_sharp',
+				'lua',
+				'python',
+                'rust',
+				'vim',
+				'vimdoc',
+				'query',
+				'markdown',
+				'markdown_inline'
+			}
+		}
+	end
+}
