4c5b35dfcf401cf9a85bb6e9b35afe399706e432
tomasz-worobiec <tomasz.worobiec@protonmail.com>
2024-11-24 19:24:48 +0100

    [mini.base16] Initialize plugin with gruvbox material dark medium theme

---
+++ lua/tom/plugins/mini-base16.lua
@@ -0,0 +1,26 @@
+return {
+    'echasnovski/mini.base16',
+    version = false,
+    config = function ()
+        require('mini.base16').setup {
+            palette = {
+                base00 = '#292828',
+                base01 = '#32302f',
+                base02 = '#504945',
+                base03 = '#665c54',
+                base04 = '#bdae93',
+                base05 = '#ddc7a1',
+                base06 = '#ebdbb2',
+                base07 = '#fbf1c7',
+                base08 = '#ea6962',
+                base09 = '#e78a4e',
+                base0A = '#d8a657',
+                base0B = '#a9b665',
+                base0C = '#89b482',
+                base0D = '#7daea3',
+                base0E = '#d3869b',
+                base0F = '#bd6f3e'
+            }
+        }
+    end
+}
