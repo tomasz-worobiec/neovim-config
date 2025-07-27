3de1748f3d302794d9b1a4018958f21c1bb5f610
tomasz-worobiec <tomasz.worobiec@protonmail.com>
2025-07-27 10:58:05 +0000

    [flake] Add initial flake

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in
+  {
+    devShells.${system}.default = pkgs.mkShell {
+      packages = with pkgs; [
+        lua-language-server
+        neovim
+      ];
+    };
+  };
+}
+
