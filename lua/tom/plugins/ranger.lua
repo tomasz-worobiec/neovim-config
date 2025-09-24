2b0f9103eb90708e32de4ac7f8f9253ece685297
Tomasz Worobiec <tomasz.worobiec@protonmail.com>
2025-09-24 18:06:28 +0200

    [ranger] Adjust height

--- lua/tom/plugins/ranger.lua
+++ lua/tom/plugins/ranger.lua
@@ -1,8 +1,14 @@
 return {
     'kelly-lin/ranger.nvim',
     config = function()
-        require("ranger-nvim").setup({ replace_netrw = true })
-            vim.api.nvim_set_keymap("n", "<leader>t", "", {
+        require("ranger-nvim").setup({
+            replace_netrw = true,
+            ui = {
+                height = 0.95
+            }
+        })
+
+        vim.api.nvim_set_keymap("n", "<leader>t", "", {
             noremap = true,
             callback = function()
                 require("ranger-nvim").open(true)
